5ee469e1879ba085eb6ddc7e10c4660bfce58929
Rostislav Futornoy <gamasha2234@gmail.com>
2018-09-15 16:03:14 +0300

    Added a separate editable method

--- components/view.js
+++ components/view.js
@@ -1,98 +1,101 @@
-
-
 function View(model) {
-	this.model = model;
-	this.countSubject = new Event();
-	this.init();
+    this.model = model;
+    this.countSubject = new Event();
+    this.init();
 }
 
 View.prototype = {
-	init: function() {
-		this.table = this.selector('.table');
+    init: function() {
+        this.table = this.selector('.table');
 
-		window.addEventListener('load', this.renderTable.bind(this));
-		window.addEventListener('click', this.renderTable.bind(this));
-		this.model.countSubject.on(() => this.refreshTable());
+        window.addEventListener('load', this.renderTable.bind(this));
+        window.addEventListener('click', this.renderTable.bind(this));
+        window.addEventListener('click', this.editTable.bind(this))
+        this.model.countSubject.on(() => this.refreshTable());
 
-		this.refreshTable()
+        this.refreshTable()
 
-		return this;
-	},
+        return this;
+    },
 
-	renderTable: function() {
-		var that = this;
-		if(localStorage.getItem('table')) {
-			document.querySelector('.table').addEventListener('click', (e) => {
-				if(!e.target.dataset.set) {
-					if(e.target.nodeName == 'TD' || e.target.nodeName == 'TR') {
-						var row = e.target.dataset.row
-						var column = e.target.dataset.column
-						var table = JSON.parse(localStorage.getItem('table'))
-						var input = document.createElement('input')
-						var text = e.target.textContent;
-						input.type = 'text'
-						input.value = text;
-						input.autofocus = 'autofocus'
-						// input.value = table[row][column];
+    // Edit table
+    editTable: function() {
+    	var that = this;
+    	
+        if (localStorage.getItem('table')) {
+            document.querySelector('.table').addEventListener('click', (e) => {
+                if (!e.target.dataset.set) {
+                    if (e.target.nodeName == 'TD' || e.target.nodeName == 'TR') {
+                        var row = e.target.dataset.row
+                        var column = e.target.dataset.column
+                        var table = JSON.parse(localStorage.getItem('table'))
+                        var input = document.createElement('input')
+                        var text = e.target.textContent;
+                        input.type = 'text'
+                        input.value = text;
+                        input.autofocus = 'autofocus'
 
-						e.target.dataset.set = true;
-						e.target.textContent = ''
+                        e.target.dataset.set = true;
+                        e.target.textContent = ''
 
-						e.target.appendChild(input)
+                        e.target.appendChild(input)
 
-						input.addEventListener('keyup', function(e) {
-							if(e.code === 'Enter') { 
-								table[row][column] = input.value 
-								that.countSubject.fire();
-								localStorage.setItem('table', JSON.stringify(table))
-								that.countSubject.fire()
-							}							
-						})
+                        input.addEventListener('keyup', function(e) {
+                            if (e.code === 'Enter') {
+                                table[row][column] = input.value
+                                that.countSubject.fire();
+                                localStorage.setItem('table', JSON.stringify(table))
+                                that.countSubject.fire()
+                            }
+                        })
 
-						localStorage.setItem('table', JSON.stringify(table))
-					}
-				} 
-		})
-			
-		} else {
-			var w = prompt("Введите ширину таблицы(в ячейках): ", '')
-			var h = prompt("Введите высоту таблицы(в ячейках): ", '')
+                        localStorage.setItem('table', JSON.stringify(table))
+                    }
+                }
+            })
 
-			localStorage.setItem('table', JSON.stringify(renderArrayOfTable(w, h)))
-		}
-	},
+        }
+    },
 
-	// render table from this.model.table
-	refreshTable: function() {
-		this.model.getTable()
+    // render new Table
+    renderTable: function() {
 
-		var table = document.createElement('table')
+        if(!localStorage.getItem('table')) {
+            var w = prompt("Введите ширину таблицы(в ячейках): ", '')
+            var h = prompt("Введите высоту таблицы(в ячейках): ", '')
 
-		this.table.innerHTML = ''
-		
-		for(var w = 0; w < this.model.table.length; w++) {
-			var tr = document.createElement('tr');
-			for(var h = 0; h < this.model.table[w].length; h++) {
-				var td = document.createElement('td');
-				td.dataset.row = w;
-				td.dataset.column = h;
-				td.textContent = this.model.table[w][h]
-				tr.appendChild(td)
-			}
-			table.appendChild(tr)
-		}
+            localStorage.setItem('table', JSON.stringify(renderArrayOfTable(w, h)))
 
-		this.table.appendChild(table)
-	},
+        }
+    },
 
-	// method for search a element in DOM
-	selector: function(selector, tobj) {
-		let that = document || tobj 
+    // render table from this.model.table
+    refreshTable: function() {
+        this.model.getTable()
 
-		return that.querySelector(selector)
-	}
+        var table = document.createElement('table')
+
+        this.table.innerHTML = ''
+
+        for (var w = 0; w < this.model.table.length; w++) {
+            var tr = document.createElement('tr');
+            for (var h = 0; h < this.model.table[w].length; h++) {
+                var td = document.createElement('td');
+                td.dataset.row = w;
+                td.dataset.column = h;
+                td.textContent = this.model.table[w][h]
+                tr.appendChild(td)
+            }
+            table.appendChild(tr)
+        }
+
+        this.table.appendChild(table)
+    },
+
+    // method for search a element in DOM
+    selector: function(selector, tobj) {
+        let that = document || tobj
+
+        return that.querySelector(selector)
+    }
 }
-
-
-
-
