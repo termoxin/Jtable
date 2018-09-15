9e0eb20483ddaf09d7d7f7781e676a11b68eb464
Rostislav Futornoy <gamasha2234@gmail.com>
2018-09-15 15:37:24 +0300

    Add edit of item from input

--- components/view.js
+++ components/view.js
@@ -14,26 +14,45 @@ View.prototype = {
 		window.addEventListener('click', this.renderTable.bind(this));
 		this.model.countSubject.on(() => this.refreshTable());
 
+		this.refreshTable()
+
 		return this;
 	},
 
 	renderTable: function() {
+		var that = this;
 		if(localStorage.getItem('table')) {
 			document.querySelector('.table').addEventListener('click', (e) => {
 				if(!e.target.dataset.set) {
-					var row = e.target.dataset.row
-					var column = e.target.dataset.column
-					var table = JSON.parse(localStorage.getItem('table'))
+					if(e.target.nodeName == 'TD' || e.target.nodeName == 'TR') {
+						var row = e.target.dataset.row
+						var column = e.target.dataset.column
+						var table = JSON.parse(localStorage.getItem('table'))
+						var input = document.createElement('input')
+						var text = e.target.textContent;
+						input.type = 'text'
+						input.value = text;
+						input.autofocus = 'autofocus'
+						// input.value = table[row][column];
 
-					e.target.dataset.set = true;
+						e.target.dataset.set = true;
+						e.target.textContent = ''
 
-					table[row][column] = prompt("Введите значение: ")
+						e.target.appendChild(input)
 
-					localStorage.setItem('table', JSON.stringify(table))
-				}
+						input.addEventListener('keyup', function(e) {
+							if(e.code === 'Enter') { 
+								table[row][column] = input.value 
+								that.countSubject.fire();
+								localStorage.setItem('table', JSON.stringify(table))
+								that.countSubject.fire()
+							}							
+						})
+
+						localStorage.setItem('table', JSON.stringify(table))
+					}
+				} 
 		})
-
-		this.countSubject.fire();
 			
 		} else {
 			var w = prompt("Введите ширину таблицы(в ячейках): ", '')
